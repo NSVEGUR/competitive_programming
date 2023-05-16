126051f0cf866c22f387e311d508504dd9edf745
Nagasai Vegur <nsvegur@gmail.com>
2023-05-17 00:03:34 +0530

    initial commit day-1

---
+++ daily/2.roman_to_int.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+// I, V, X, L, C, D, M
+class Solution{
+	public:
+		int romanToInt(string s){
+			string temp = "";
+			for(int i = 0; i <  s.length(); i++){
+				
+			}
+		}
+};
